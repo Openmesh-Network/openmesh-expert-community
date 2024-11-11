d7d4ec45ec9be301195a2da579da907743f0e5c9
Plopmenz <plopmenz@gmail.com>
2024-11-11 09:48:43 +0100

    update logo

--- app/head.tsx
+++ app/head.tsx
@@ -4,14 +4,7 @@ export default function Head() {
       <title>Openmesh Community</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Openmesh Community" />
-      <link
-        rel="icon"
-        href={`${
-          process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
-            ? process.env.NEXT_PUBLIC_BASE_PATH
-            : ''
-        }/images/openmesh-blue.png`}
-      />
+      <link rel="icon" href={`${process.env.NEXT_PUBLIC_ENVIRONMENT === "PROD" ? process.env.NEXT_PUBLIC_BASE_PATH : ""}/images/openmesh.svg`} />
     </>
-  )
+  );
 }
